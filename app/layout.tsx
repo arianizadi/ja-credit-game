d651c819854c0f762d4cc9242f4262e173efde97
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2026-01-05 15:43:27 -0800

    Add tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script
+          src="https://umami.arianizadi.com/script.js"
+          data-website-id="af999df7-8a30-4b28-8748-b7f9ff36a196"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
